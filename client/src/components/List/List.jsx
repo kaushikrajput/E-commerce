bd231a20022122c39e930cbb331a0e6dc57d7454
kaushik <kaushikrajput0926@gmail.com>
2023-09-01 14:37:40 +0530

    commit

---
+++ client/src/components/List/List.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./list.scss";
+import Card from "../Card/Card";
+
+const List = () => {
+  const data = [
+    {
+      id: 1,
+      img: "https://i.pinimg.com/564x/b7/9d/4c/b79d4c3e6cd33900d22321c294e409bd.jpg",
+      img2: "https://i.pinimg.com/564x/73/3d/c9/733dc9794854749d0772fe88d519b0c5.jpg",
+      title: "Pants",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 2,
+      img: "https://i.pinimg.com/564x/34/56/7e/34567e7b799dccac642d9a4cea8c9fdd.jpg",
+      title: "Sweatshirt",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 3,
+      img: "https://i.pinimg.com/564x/b8/73/14/b873142282b09b2b8a5554f01d605c61.jpg",
+      title: "Sweatshirt",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 4,
+      img: "https://i.pinimg.com/736x/4e/7d/1f/4e7d1f3e6456d3648fc0c1268ffdf99f.jpg",
+      title: "Turtle Neck",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+  ];
+  return (
+    <div className="list">
+      {data.map((item) => (
+        <Card item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
+
+export default List;
--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./product.scss";
 
 const Product = () => {
+  const [selectedImg, setSelectedImg] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+  const images = [
+    "https://i.pinimg.com/564x/d6/98/40/d698409f4efc38f857b0017fbc4e2983.jpg",
+    "https://i.pinimg.com/564x/e2/7f/72/e27f7295fd95193e1452b535d88a24e8.jpg",
+  ];
   return (
-    <div>Product</div>
-  )
-}
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
+          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <h1>Title</h1>
+        <span className="price">$199</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
+          in, aspernatur vero aliquid sapiente nulla laboriosam ratione
+          molestiae hic voluptatum mollitia odio natus, asperiores omnis iste
+          est dignissimos ipsa suscipit ducimus facere! Doloremque deserunt
+          quasi iusto sit commodi, ipsum in.
+        </p>
+        <div className="quantity">
+          <button
+            onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}
+          >
+            -
+          </button>
+          {quantity}
+          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+        </div>
+        <button className="add">
+          <i class="fa-solid fa-cart-plus"></i>
+          add to cart
+        </button>
+        <div className="links">
+          <div className="item">
+            add to wishlist
+            <i class="fa-regular fa-heart"></i>
+          </div>
+          <div className="item">
+            add to compare
+            <i class="fa-solid fa-scale-balanced"></i>
+          </div>
+        </div>
+        <div className="info">
+          <span>Vendor: Polo</span>
+          <span>Product Type: T-Shirt</span>
+          <span>Tag: T-Shirt, Women, Top</span>
+        </div>
+        <hr />
+        <div className="info">
+          <span>DESCRIPTION</span>
+          <hr />
+          <span>ADDITIONAL INFORMATION</span>
+          <hr />
+          <span>FAQ</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Product
+export default Product;
